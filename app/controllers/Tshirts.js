1ee5acaa88f95d4a891a6d80c79ff8998a96893c
jonasa64 <hvidovrefifa@gmail.com>
2020-04-17 17:20:44 +0200

    Update Tshirts.js

--- app/controllers/Tshirts.js
+++ app/controllers/Tshirts.js
@@ -1,89 +1,102 @@
 const shirt = require('../models/Tshirt');
 
-exports.findAll = (req, res) => {
-    shirt.findAll()
-        .then(shirts => {
-            if(!shirts){
-                return res.status(500).json({msg:'failed to find any t-shits'})
-            }
-            res.status(200).send(shirts);
-        })
-        .catch(err => console.log(err))
-};
+exports.create = (req, res) => {
 
+    const newShirt = {
+        Name: req.body.name,
+        Color: req.body.color,
+        Price: req.body.price,
+        Description: req.body.description,
+        ImageUrl: req.body.imageUrl
+
+
+    };
+
+    shirt.create(newShirt)
+        .then(data => {
+            res.send(data);
+        }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occuredd while creating the shirt"
+        });
+    });
+
+}
+
+
+exports.findAll = (req, res) => {
+
+ shirt.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving shirts"
+            });
+        });
+}
 
 exports.findById = (req, res) => {
-    const id = req.params.id;
-    shirt.findById(id)
-        .then(shirt => {
-            if(!shirt){
-                return res.status(500).json({msg: 'failed to find a t-shit with the give id'});
-            }
 
-            res.status(200).send(shirt);
+    const id = req.params.id;
+    shirt.findByPk(id)
+        .then(data => {
+            res.send(data);
         })
-        .catch(err => console.log(err));
-}
-
-exports.create = (req, res) => {
-    const name = req.body.name;
-    const  color = req.body.color;
-    const price = req.body.price;
-    const description = req.body.description;
-    const  imageUrl = req.body.imageUrl;
-
-    const checkForEmpty = [name, color, price, description, imageUrl];
-
-    if (checkForEmpty.filter(el => el === '').length > 0){
-        return res.status(400).json({msg: 'Please fill out all the fields'})
-    }
-
-    shirt.create({
-        Name: name,
-        Color: color,
-        Price: price,
-        Description: description,
-        ImageUrl: imageUrl
-    }).then(shirt => res.status(201).json({msg: 't-shirt created successfully'}))
-        .catch(err => console.log(err));
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error retrieving shirt with id= ' + id
+            });
+        });
 
 }
 
-exports.update = (res, res) => {
+
+exports.update = (req, res) => {
     const id = req.params.id;
-    const name = req.body.name;
-    const  color = req.body.color;
-    const price = req.body.price;
-    const description = req.body.description;
-    const  imageUrl = req.body.imageUrl;
 
-    const checkForEmpty = [name, color, price, description, imageUrl];
-
-    if (checkForEmpty.filter(el => el === '').length > 0){
-        return res.status(400).json({msg: 'Please fill out all the fields'})
-    }
-
-    shirt.findById(id).then(shirt => {
-        shirt.Name = name;
-        shirt.Color = color;
-        shirt.Price = price;
-        shirt.Description = description;
-        shirt.ImageUrl = imageUrl;
-        return shirt.save();
-    }).then(result => res.status(200).json({msg: 't-shirt is updated'}))
-        .catch(err => console.log(err));
+    shirt.update(req.body, {
+        where: { id: id }
+    }).then(num => {
+        if (num == 1) {
+            res.send({
+                message: "Shirt was updated successfully"
+            });
+        } else {
+            res.send({
+                message: `Cannot update Shirt with id=${id}. Maybe Shirt was not found or req.body is empty!`
+            })
+        }
+    }).catch(err => {
+        res.send(500).send({
+            message: "Error updating shirt with id=" + id
+        });
+    });
 }
+
 
 exports.delete = (req, res) => {
+
     const id = req.params.id;
-    shirt.findById(id)
-        .then(shirt => {
-            if(!shirt){
-                return res.status(500).json({msg : 'failed to find a t-shit with the give id'});
-            }
-            return shirt.destroy();
-        }).then(restult => res.status(200).json({msg: 't-shirt is deleted '}))
-        .catch(err => console.log(err));
+
+    shirt.destroy({
+        where: { id: id }
+    }).then(num => {
+        if (num == 1) {
+            res.send({
+                message: "shirt was deleted successfully!"
+            })
+        } else {
+            res.send({
+                message: `Cannot delete shirt with id=${id}. Maybe shirt was not found!`
+            })
+        }
+    })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete shirt with id=" + id
+            })
+        })
+
 }
-
-
