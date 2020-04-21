eeaa64ef88c0afa4d7b5c5fa0ea2e422f56f3f44
Henna <qureshi.henna@gmail.com>
2020-04-21 22:56:13 +0200

    Customer login code debug

--- app/controllers/Tshirts.js
+++ app/controllers/Tshirts.js
@@ -8,8 +8,6 @@ exports.create = (req, res) => {
         Price: req.body.price,
         Description: req.body.description,
         ImageUrl: req.body.imageUrl
-
-
     };
 
     shirt.create(newShirt)
--- app/routes/Sizes.js
+++ app/routes/Sizes.js
@@ -1,10 +1,16 @@
 module.exports = app => {
     const express = require('express');
     const router = express.Router();
+
+    var cors = require('cors');
+
     const sizeController = require('../controllers/Sizes');
-
-
-
+    app.all('/', function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
+    
     router.get('/:id', sizeController.find);
 
     router.post('/', sizeController.create);
